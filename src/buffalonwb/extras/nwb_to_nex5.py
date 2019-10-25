b0fb74642780f0341d9966511192cb014560a6d4
Ryan Ly <rly@lbl.gov>
2019-10-25 13:43:18 -0700

    Make nwb to nex5 work for reading in offline sorter

--- src/buffalonwb/extras/nwb_to_nex5.py
+++ src/buffalonwb/extras/nwb_to_nex5.py
@@ -1,19 +1,29 @@
-from nexfile import nexfile
+from nexfile import nexwriter2
 import pynwb
-import numpy as np
+import sys
+from tqdm import trange
+
 
 def nwb_to_nex5(nwb_path, nex5_path):
     with pynwb.NWBHDF5IO(nwb_path, 'r') as io:
         nwb = io.read()
-        nChannels = nwb.acquisition['ElectricalSeries'].data.shape[1]
-        conversion = nwb.acquisition['ElectricalSeries'].conversion*1000
-        timestampFrequency = nwb.acquisition['ElectricalSeries'].rate
-        writer = nexfile.NexWriter(timestampFrequency, useNumpy=True)
-        for ch in np.arange(nChannels):
+        raw_data_name = 'raw_ephys'
+        nChannels = nwb.acquisition[raw_data_name].data.shape[1]
+        timestampFrequency = nwb.acquisition[raw_data_name].rate
+        writer = nexwriter2.NexWriter2(timestampFrequency, useNumpy=True)
+        for ch in trange(nChannels, desc='writing channels'):
             writer.AddContVarWithSingleFragment(
                 name='channel_'+str(ch),
                 timestampOfFirstDataPoint=0,
                 SamplingRate=timestampFrequency,
-                values=nwb.acquisition['ElectricalSeries'].data[:, ch]*conversion
+                values=nwb.acquisition[raw_data_name].data[:, ch]  # data is in int16
             )
-        writer.WriteNex5File(nex5_path, saveContValuesAsFloats=1)
+
+        # NEX5 stores data in millivolts, not volts
+        conversion = nwb.acquisition[raw_data_name].conversion*1000
+
+        writer.WriteNex5File(nex5_path, conversion=conversion)
+
+
+if __name__ == '__main__':
+    nwb_to_nex5(sys.argv[1], sys.argv[2])
