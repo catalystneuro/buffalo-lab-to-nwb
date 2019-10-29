31e7828f92dbca99c1d47e5949ff37843bc90225
Ryan Ly <rly@lbl.gov>
2019-10-29 13:40:40 -0700

    supply default name for electrical series

--- src/buffalonwb/extras/nwb_to_nex5.py
+++ src/buffalonwb/extras/nwb_to_nex5.py
@@ -1,13 +1,12 @@
 from nexfile import nexwriter2
 import pynwb
-import sys
 from tqdm import trange
 import numpy as np
 import argparse
 import datetime
 
 
-def nwb_to_nex5(nwb_path, elecseries_name, nex5_path):
+def nwb_to_nex5(nwb_path, nex5_path, elecseries_name='ElectricalSeries'):
     """
     Write the given NWB ElectricalSeries to a NEX5 file.
     :param nwb_path: path to the NWB file.
@@ -18,13 +17,13 @@ def nwb_to_nex5(nwb_path, elecseries_name, nex5_path):
     with pynwb.NWBHDF5IO(nwb_path, 'r') as io:
         nwb = io.read()
         if elecseries_name not in nwb.acquisition:
-            raise Exception('NWB file %s does not have an acquisition named %s.' % (nwb_path, elecseries_name))
+            raise Exception('NWB file %s does not have an acquisition named "%s".' % (nwb_path, elecseries_name))
 
         elecseries = nwb.acquisition[elecseries_name]
         if not isinstance(elecseries, pynwb.ecephys.ElectricalSeries):
-            raise Exception('Acquisition %s must be of type ElectricalSeries.' % (elecseries_name))
+            raise Exception('Acquisition "%s" must be of type ElectricalSeries.' % (elecseries_name))
         if elecseries.data.dtype is not np.dtype(np.int16):
-            raise Exception('Acquisition %s must have int16 data.' % (elecseries_name))
+            raise Exception('Acquisition "%s" must have int16 data.' % (elecseries_name))
 
         num_channels = elecseries.data.shape[1]
         start_time = nwb.timestamps_reference_time
@@ -34,12 +33,11 @@ def nwb_to_nex5(nwb_path, elecseries_name, nex5_path):
         print('Found ElectricalSeries "%s" data:' % elecseries_name)
         print('num samples: \t\t\t%d' % elecseries.data.shape[0])
         print('num channels: \t\t\t%d' % num_channels)
-        print('start time: \t\t\t%s (%f seconds)' % (start_time, start_time.timestamp()))
         print('sampling rate: \t\t\t%f' % timestamp_freq)
         print('AD to mV conversion factor: \t%f' % conversion)
         print('elecseries starting time: \t%s (%f seconds)'
               % (datetime.datetime.fromtimestamp(elecseries.starting_time / 1000), elecseries.starting_time / 1000))
-        breakpoint()
+        print('timestamps reference time: \t\t\t%s (%f seconds)' % (start_time, start_time.timestamp()))
 
         # use modified nexwriter in order to handle AD (int16) data and given conversion factor
         writer = nexwriter2.NexWriter2(timestamp_freq, useNumpy=True)
@@ -60,13 +58,14 @@ def main():
         "nwb_path", help="The path to the NWB file."
     )
     parser.add_argument(
-        "elecseries_name", help="The name of the ElectricalSeries in the NWB file to be written to the NEX5 file."
+        "elecseries_name",
+        help="The name of the ElectricalSeries in the NWB file to be written to the NEX5 file.",
     )
     parser.add_argument(
         "nex5_path", help="The path to the NEX5 file to be written."
     )
     args = parser.parse_args()
-    nwb_to_nex5(args.nwb_path, args.elecseries_name, args.nex5_path)
+    nwb_to_nex5(args.nwb_path, args.nex5_path, args.elecseries_name)
 
 
 if __name__ == '__main__':
