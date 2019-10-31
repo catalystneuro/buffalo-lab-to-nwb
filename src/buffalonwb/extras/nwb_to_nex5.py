bc417ff48501311dc7e90b858adcaeb47684d2e7
Ryan Ly <rly@lbl.gov>
2019-10-31 14:42:29 -0700

    Use supplied start time and num channels for electrical series

--- src/buffalonwb/extras/nwb_to_nex5.py
+++ src/buffalonwb/extras/nwb_to_nex5.py
@@ -26,25 +26,27 @@ def nwb_to_nex5(nwb_path, nex5_path, elecseries_name='ElectricalSeries'):
             raise Exception('Acquisition "%s" must have int16 data.' % (elecseries_name))
 
         num_channels = elecseries.data.shape[1]
-        start_time = nwb.timestamps_reference_time
+        # use electricalseries start time, which is relative to timestamps_reference_time
+        start_time = elecseries.starting_time
         timestamp_freq = elecseries.rate
         conversion = elecseries.conversion*1000  # NEX5 stores data in millivolts, not volts
 
         print('Found ElectricalSeries "%s" data:' % elecseries_name)
-        print('num samples: \t\t\t%d' % elecseries.data.shape[0])
-        print('num channels: \t\t\t%d' % num_channels)
-        print('sampling rate: \t\t\t%f' % timestamp_freq)
+        print('Num channels: \t\t\t%d' % num_channels)
+        print('Num samples: \t\t\t%d' % elecseries.data.shape[0])
+        print('Sampling rate: \t\t\t%f Hz' % timestamp_freq)
+        print('Total time: \t\t\t%f seconds' % (elecseries.data.shape[0] / timestamp_freq))
         print('AD to mV conversion factor: \t%f' % conversion)
-        print('elecseries starting time: \t%s (%f seconds)'
-              % (datetime.datetime.fromtimestamp(elecseries.starting_time / 1000), elecseries.starting_time / 1000))
-        print('timestamps reference time: \t\t\t%s (%f seconds)' % (start_time, start_time.timestamp()))
+        print('ElectricalSeries starting time: %f seconds' % start_time)
+        print('Timestamps reference time: \t%s' % nwb.timestamps_reference_time)
+        print('')
 
         # use modified nexwriter in order to handle AD (int16) data and given conversion factor
         writer = nexwriter2.NexWriter2(timestamp_freq, useNumpy=True)
-        for ch in trange(2, desc='Writing channels to NEX5 file'):
+        for ch in trange(num_channels, desc='Writing channels to NEX5 file'):
             writer.AddContVarWithSingleFragment(
                 name='channel_'+str(ch),
-                timestampOfFirstDataPoint=start_time.timestamp(),
+                timestampOfFirstDataPoint=start_time,
                 SamplingRate=timestamp_freq,
                 values=elecseries.data[:, ch]
             )
